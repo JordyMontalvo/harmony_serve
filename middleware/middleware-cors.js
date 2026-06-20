b89158d5ab7ff34f749bbc1b555dce4cafe678f7
Carlos <charliecumparamirez@gmail.com>
2026-06-20 13:13:49 -0500

    inicio sesion admin

--- middleware/middleware-cors.js
+++ middleware/middleware-cors.js
@@ -4,6 +4,7 @@ const allowedOrigins = [
   'http://localhost:8081',
   'http://localhost:8082',
   'http://localhost:8080',
+  'http://localhost:8083',
   'http://localhost:3000',
   
   // Producción - Vercel
